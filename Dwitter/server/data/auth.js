f35c17ebfdc9b2b842a057bff122bb1fb6cb62c3
Dayong Hyun <gusek93@naver.com>
2021-08-25 11:39:35 +0900

    구현 내용 정리

---
+++ Dwitter/server/data/auth.js
@@ -0,0 +1,25 @@
+// abcd1234: $2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm
+let users = [
+  {
+    id: '1',
+    username: 'bob',
+    password: '$2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm',
+    name: 'Bob',
+    email: 'bob@gmail.com',
+    url: 'https://img.icons8.com/material-outlined/80/000000/cloud-network.png',
+  },
+];
+
+export async function findByUsername(username) {
+  return users.find((user) => user.username === username);
+}
+
+export async function findById(id) {
+  return users.find((user) => user.id === id);
+}
+
+export async function createUser(user) {
+  const created = { ...user, id: Date.now().toString() };
+  users.push(created);
+  return created.id;
+}
